a5e4fe5f4c3ba84ff80e3f477372a1e2e1abab45
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-08-11 12:47:16 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,113 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-        });
-        
-        function validarFormulario(evento) {
-          evento.preventDefault();
-          var usuario = document.getElementById('Inputuser').value;
-          if(usuario.length == 0) {
-            alert('No has escrito nada en el usuario');
-            return;
-          };
-          var clave = document.getElementById('Password').value;
-          if (clave.length < 6) {
-            alert('La clave tiene que tener 6 digitos ');
-            return;
-         
-          };
-          this.submit();
-          
-         
-  };
-  function onSignIn(googleUser) {
-          var profile = googleUser.getBasicProfile();
-          console.log('ID: ' + profile.getId()); 
-          console.log('Name: ' + profile.getName());
-          console.log('Image URL: ' + profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail());
-  };
-  document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-        });
-        
-        function validarFormulario(evento) {
-          evento.preventDefault();
-          var usuario = document.getElementById('Inputuser').value;
-          if(usuario.length == 0) {
-            alert('No has escrito nada en el usuario');
-            return;
-          };
-          var clave = document.getElementById('Password').value;
-          if (clave.length < 6) {
-            alert('La clave tiene que tener 6 digitos ');
-            return;
-         
-          };
-          this.submit();
-          
-         
-  };
-  function onSignIn(googleUser) {
-          var profile = googleUser.getBasicProfile();
-          console.log('ID: ' + profile.getId()); 
-          console.log('Name: ' + profile.getName());
-          console.log('Image URL: ' + profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail());
-  };
-  document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-        });
-        
-        function validarFormulario(evento) {
-          evento.preventDefault();
-          var usuario = document.getElementById('Inputuser').value;
-          if(usuario.length == 0) {
-            alert('No has escrito nada en el usuario');
-            return;
-          };
-          var clave = document.getElementById('Password').value;
-          if (clave.length < 6) {
-            alert('La clave tiene que tener 6 digitos ');
-            return;
-         
-          };
-          this.submit();
-          
-         
-  };
-  function onSignIn(googleUser) {
-          var profile = googleUser.getBasicProfile();
-          console.log('ID: ' + profile.getId()); 
-          console.log('Name: ' + profile.getName());
-          console.log('Image URL: ' + profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail());
-  };
-  document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-        });
-        
-        function validarFormulario(evento) {
-          evento.preventDefault();
-          var usuario = document.getElementById('Inputuser').value;
-          if(usuario.length == 0) {
-            alert('No has escrito nada en el usuario');
-            return;
-          };
-          var clave = document.getElementById('Password').value;
-          if (clave.length < 6) {
-            alert('La clave tiene que tener 6 digitos ');
-            return;
-         
-          };
-          this.submit();
-          
-         
-  };
-  function onSignIn(googleUser) {
-          var profile = googleUser.getBasicProfile();
-          console.log('ID: ' + profile.getId()); 
-          console.log('Name: ' + profile.getName());
-          console.log('Image URL: ' + profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail());
-  };
-            
+let idproducto = [];
+var categoriaid = localStorage.getItem("catID");
+async function listado() {
+  fetch(
+    `https://japceibal.github.io/emercado-api/cats_products/` +
+      categoriaid +
+      `.json`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      listautos(data.products);
+    });
+}
+
+function listautos(idproducto) {
+  console.log(idproducto);
+  let htmlContentToAppend = "";
+  for (let i = 0; i < idproducto.length; i++) {
+    const producto = idproducto[i + 0];
+    
+                 
+    htmlContentToAppend +=
+      `
+                    <div class="row">
+                        <div class="col-4">
+                            <img src=" ${producto.image}  " alt="` +idproducto.ID +`" class="img-thumbnail">
+                     </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-2">  ${producto.name} -   ${producto.currency}  ${producto.cost}  </h4>                                
+                                <small class="text-muted">${producto.soldCount} Vendidos</small>
+                            </div>
+                            <p class="mb-9">${producto.description}</p>
+                        </div>
+                    </div>
+                </div>
+                
+                `;
+    document.getElementById("productos").innerHTML = htmlContentToAppend;
+    }
+    
+}
+
+listado();
+listautos();
