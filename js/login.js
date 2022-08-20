fb68d4ce84d6b44a430df9711426c3f3fc965998
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-08-20 12:31:42 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -46,24 +46,22 @@ function onSignIn(googleUser) {
 
 function handleCredentialResponse(response) {
   const responsePayload = decodeJwtResponse(response.credential);
-  window.location.href = "index.html";
   console.log("ID: " + responsePayload.sub);
   console.log('Full Name: ' + responsePayload.name);
   console.log('Given Name: ' + responsePayload.given_name);
   console.log('Family Name: ' + responsePayload.family_name);
   console.log("Image URL: " + responsePayload.picture);
   console.log("Email: " + responsePayload.email);
-  console.log(document.cookie)
 }
 
-function parseJwt (token) {
+function decodeJwtResponse (token) {
   var base64Url = token.split('.')[1];
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
-
   return JSON.parse(jsonPayload);
 };
+
 
 
