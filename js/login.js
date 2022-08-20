c7a0a5613c64b472052292f25b9b9c762984502c
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-08-20 14:35:42 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,6 +5,24 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("formulario").addEventListener('submit', validarFormulario); 
 
       });
+
+
+function handleCredentialResponse(response) {
+  const responsePayload = decodeJwtResponse(response.credential);
+  localStorage.setItem("datos", JSON.stringify(responsePayload));
+  window.location.href = "index.html";
+  
+}
+
+function decodeJwtResponse (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload);
+};
+
   
       function user(id) {
         sessionStorage.setItem("user", id);
@@ -36,20 +54,6 @@ document.addEventListener("DOMContentLoaded", function () {
   
 
 
-function handleCredentialResponse(response) {
-  const responsePayload = decodeJwtResponse(response.credential);
-  localStorage.setItem("datos", JSON.stringify(responsePayload));
-  window.location.href = "index.html";
-  
-}
 
-function decodeJwtResponse (token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-  return JSON.parse(jsonPayload);
-};
 
 
