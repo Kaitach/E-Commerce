60ebb7cc049d07ef55323975dbe3ae6a38f04b3d
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-08-20 17:37:04 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -5,7 +5,7 @@
     
   <div class="mb-2"  class="container" >
   
-       <h1 class="mb-4" >  Bienvenido usuario: ` + hola.name + `   <img  id="img1"  class="perfilimg" src=` + hola.picture + ` > </button> </h1>
+       <h1 class="mb-4" >  Bienvenido usuario: ` + hola.name + `   <img  id="img1"  class="perfilimg" src=` + hola.foto + ` > </button> </h1>
        <h1 class="mb-4" >  Su correo es: ` + hola.email + `  </h1>    
        <h1 class="mb-4" >  Su  id : ` + hola.sub + `  </h1>     
       <h1 class="mb-2" >  Esperamos su estadia en nuestra web sea grata </h1>  
