8097cab99e1d67812274c88cbbcbc444de83e8dd
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-08-20 14:12:54 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,31 +1,12 @@
 
-//  function perfilimg(id) {
-//     sessionStorage.setItem("foti", id);
-//   }
-// var perfilimg = sessionStorage.getItem("foti");
-
-
-    
-    
+        
 let  hola = JSON.parse(sessionStorage.getItem('datos'));
 document.getElementById('Perfil').innerHTML = `     
   
 <div class="mb-2"  class="container" >
 
-     <h1 class="mb-4" >  Bienvenido usuario: ` + hola.usuario +  ` <button id="changeBackground">  <img  id="img1"  class="perfilimg" src="img/monosonrie.jpeg" > </button> </h1>  
+     <h1 class="mb-4" >  Bienvenido usuario: ` + hola.name +  `   <img  id="img1"  class="perfilimg" src=`+hola.picture+` > </button> </h1>
+     <h1 class="mb-4" >  Su correo es: ` + hola.email +  `  </h1>    
     <h1 class="mb-2" >  Esperamos su estadia en nuestra web sea grata </h1>  
   </div>
   `
-
-
-// aca tratando de hacer un cambio de imagen con un click
-
-  // const cambiodefoto = document.getElementById("changeBackground");
-
-// changeBackground.addEventListener("click", () => {
-        
-//         document.getElementById("img1").src="img/monosonrie.jpeg";
-//         sessionStorage.setItem("foti", 0);
-// });
-  
-
