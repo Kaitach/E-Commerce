1ce723ab6a83a1815f5aa915b52a0d5c41546c68
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-08-20 14:43:17 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,12 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-        
-let  hola = JSON.parse(sessionStorage.getItem('datos'));
-document.getElementById('Perfil').innerHTML = `     
+  let  hola = JSON.parse(sessionStorage.getItem('datos'));
+  document.getElementById('Perfil').innerHTML = `     
+    
+  <div class="mb-2"  class="container" >
   
-<div class="mb-2"  class="container" >
-
-     <h1 class="mb-4" >  Bienvenido usuario: ` + hola.name +  `   <img  id="img1"  class="perfilimg" src=`+hola.picture+` > </button> </h1>
-     <h1 class="mb-4" >  Su correo es: ` + hola.email +  `  </h1>    
-    <h1 class="mb-2" >  Esperamos su estadia en nuestra web sea grata </h1>  
-  </div>
-  `
+       <h1 class="mb-4" >  Bienvenido usuario: ` + hola.name +  `   <img  id="img1"  class="perfilimg" src=`+hola.picture+` > </button> </h1>
+       <h1 class="mb-4" >  Su correo es: ` + hola.email +  `  </h1>    
+       <h1 class="mb-4" >  Su ID es: ` + hola.sub +  `  </h1>           
+  
+  
+      <h1 class="mb-2" >  Esperamos su estadia en nuestra web sea grata </h1>  
+    </div>
+    `
+   
+            
+});
